5ca2eda57cdb7e5c20d1b0581c40cff8ab916dcf
Spencer Lewis <spence.914@gmail.com>
2023-10-17 15:18:17 -0700

    update assertEqual.js with tempalte literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,7 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅" + "Assertion Passed: " + actual + " === " + expected);
-  } else console.log("🛑🛑🛑" + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+console.log(assertEqual(1, 2))
