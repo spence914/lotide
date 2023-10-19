1988055ada2e55923b915cec55c04da3d90c40c8
Spencer Lewis <spence.914@gmail.com>
2023-10-19 13:36:46 -0700

    add check to see if arrays are the same length before looping through

--- eqArrays.js
+++ eqArrays.js
@@ -13,6 +13,9 @@ const assertEqual = function(actual, expected) {
 // index to see if they are exactly equal.
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -25,3 +28,5 @@ const eqArrays = function(arr1, arr2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([],[]), true)
+assertEqual(eqArrays([1,2,3],[1,2]), false)
