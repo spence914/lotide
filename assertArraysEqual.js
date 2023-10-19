2df635ac9dc1de94a59dae7af3c0e11dafd2dae7
Spencer Lewis <spence.914@gmail.com>
2023-10-19 13:57:29 -0700

    simplify if statement to remove unnecessary equality check and add curly bracket for clarity

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,9 +10,10 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  };
 };
 
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
