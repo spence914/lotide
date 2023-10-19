2aafd2bea9f6f77914376a63a90c303f0b5eb87c
Spencer Lewis <spence.914@gmail.com>
2023-10-19 13:54:09 -0700

    add assertArraysEqualFunction using eqArrays function to determine array equality and print result to console

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
@@ -8,3 +8,12 @@ const eqArrays = function(arr1, arr2) {
     }
   } return true;
 };
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
