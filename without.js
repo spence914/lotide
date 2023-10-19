60f2fc9272f312c629a933446e5c8a5f562f4447
Spencer Lewis <spence.914@gmail.com>
2023-10-19 14:04:45 -0700

    create without.js file, add necessary functions from previous exercise, add comments indicating scope of function, inputs, outputs, and initial process idea

---
+++ without.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  };
+};
+
+//Implement without function that takes an array and a set of unwanted elements to
+//remove from that array
+
+//Inputs: An array, and a set of unwanted elements
+//Outputs:  
