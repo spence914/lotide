beae16418ffe134aae94aebd4c1ef2fa82806412
Spencer Lewis <spence.914@gmail.com>
2023-10-19 14:19:35 -0700

    implement without function using a loop to find unwanted elements in the source array, and push the wanted ones to a new array, as well as add test cases to determine function is working as intended and creating a new array of wanted elements

--- without.js
+++ without.js
@@ -12,12 +12,32 @@ const eqArrays = function(arr1, arr2) {
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
 
 //Implement without function that takes an array and a set of unwanted elements to
 //remove from that array
 
-//Inputs: An array, and a set of unwanted elements
-//Outputs:  
+//Inputs: Two arrays, a complete one, and one containing unwated elements
+//Outputs: A new array containing only the wanted elements from the first array
+//Process: Loop through the array comparing the elements and if they don't match the unwanted
+//ones them pushing them into a new array
+
+const without = function(sourceArray, itemsToRemove) {
+  let wantedElements = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i] !== itemsToRemove[i]) {
+      wantedElements.push(sourceArray[i]);
+    }
+  } return wantedElements;
+};
+
+without([1, 2, 3], [1]);
+without(["1", "2", "3"], [1, 2, "3"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
