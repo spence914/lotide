dc74d4f6268e3b67396cb4a96345170f1ee2b294
Spencer Lewis <spence.914@gmail.com>
2023-10-19 15:41:47 -0700

    add code for flatten function with nested for loops that push the elements to a new flattened array, as well as some test cases to ensure correct functioning

--- flatten.js
+++ flatten.js
@@ -12,8 +12,9 @@ const eqArrays = function(arr1, arr2) {
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
 
 //Create a funciton flatten that will take an array containing elements included nested arrays
@@ -25,3 +26,19 @@ const assertArraysEqual = function(arr1, arr2) {
 //Method: I can loop through the outer array checking if each element is itself an array,
 // if it isn't I can just push it straigh to the new flattenedArray and if it is I can use
 // another loop that pushes each of those elements to the new array
+
+const flatten = function(tallArray) {
+  let flattenedArray = [];
+  for (let i = 0; i < tallArray.length; i++) {
+    if (!Array.isArray(tallArray[i])) {
+      flattenedArray.push(tallArray[i]);
+    } if (Array.isArray(tallArray[i])) {
+      for (let j = 0; j < tallArray[i].length; j++) {
+        flattenedArray.push(tallArray[i][j]);
+      }
+    }
+  } return flattenedArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([2,4,6,[2,4,6]]),[2,4,6,2,4,6]);
