973bd5f569dd76abb7e0321d549a11db36e48ee9
Spencer Lewis <spence.914@gmail.com>
2023-10-19 15:04:43 -0700

    fix logic to check if unwanted items are in source array, not other way around

--- without.js
+++ without.js
@@ -23,12 +23,15 @@ const assertArraysEqual = function(arr1, arr2) {
 //Inputs: Two arrays, a complete one, and one containing unwated elements
 //Outputs: A new array containing only the wanted elements from the first array
 //Process: Loop through the array comparing the elements and if they don't match the unwanted
-//ones them pushing them into a new array
+//ones them pushing them into a new array.
+//Above method didnt work because it only compared two elements at the same index, AI help
+// on compass recomended I use the includes method to check if the source array included any
+//of the unwanted elements so I have implemented that here
 
 const without = function(sourceArray, itemsToRemove) {
   let wantedElements = [];
   for (let i = 0; i < sourceArray.length; i++) {
-    if (sourceArray.includes(itemsToRemove[i]) === false) {
+    if (itemsToRemove.includes(sourceArray[i]) === false) {
       wantedElements.push(sourceArray[i]);
     }
   } return wantedElements;
