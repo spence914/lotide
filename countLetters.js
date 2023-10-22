f160fdb8070a46b164fdd161e39859d0d053cd32
Spencer Lewis <spence.914@gmail.com>
2023-10-22 16:30:21 -0700

    add more test cases to account for different scenarios

--- countLetters.js
+++ countLetters.js
@@ -7,12 +7,12 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(sentence) {
   const results = {};
 
-  for (const letters of sentence) {
-    if (letters !== ' ') {
-      if (results[letters]) {
-        results[letters] += 1;
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
       } else {
-        results[letters] = 1;
+        results[letter] = 1;
       }
     }
   }
@@ -22,3 +22,5 @@ const countLetters = function(sentence) {
 assertEqual(countLetters("lighthouse in the house").h , 4);
 assertEqual(countLetters("spencer ernest lewis")["s"] , 3);
 assertEqual(countLetters("spencer ernest lewis")[" "] , undefined);
+assertEqual(countLetters("      ")[" "] , undefined);
+assertEqual(countLetters("s")["s"] , 1);
