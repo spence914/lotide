a535484a5fd4bfc20cd7e1f0556e13297d9aac78
Spencer Lewis <spence.914@gmail.com>
2023-10-22 16:02:34 -0700

    add if statement such that only items that are found as keys in the itemsToCount object are counted in the results object

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -8,15 +8,16 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    };
-  }
-
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      };
+    }
+  };
   return results;
-}
+};
 
 
 const firstNames = [
