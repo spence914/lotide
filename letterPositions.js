9b965f7f8532a42103150b71e088b4cf347065ea
Spencer Lewis <spence.914@gmail.com>
2023-10-22 17:11:20 -0700

    add code to loop through given sentence, for each letter found create that key in the results object, and as the value for the object an array containing the index value in the original sentence, if the key already exists ;it pushes the next index value where the letteris found into the array

--- letterPositions.js
+++ letterPositions.js
@@ -12,18 +12,29 @@ const eqArrays = function(arr1, arr2) {
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
-
-
-
 
 
 const letterPositions = function(sentence) {
   const results = {};
+  // Use a for..of loop to loop through the given sentence, writing to the result object
+  // a key for each letter, then an array as the value for each key, inside of which is the
+  // index where the letter is found
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else results[sentence[i]] = [i];
+    }
+  }
   return results;
 };
 
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("lighthouse labs").l, [0, 11]);
 
-assertArraysEqual(letterPositions("hello").e, 1);
