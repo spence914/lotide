4a14d94daa9f45c9be02b9e24d81dcd035a2ece4
Spencer Lewis <spence.914@gmail.com>
2023-10-22 20:38:37 -0700

    add function eqObjects, assertion function, and implement test cases

---
+++ eqObjects.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  };
+
+
+
+  const eqObjects = function(object1, object2) {
+  };
+
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
