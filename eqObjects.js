63369d70c07efd0a39db71cc8d4485d9627ef567
Spencer Lewis <spence.914@gmail.com>
2023-10-24 13:08:37 -0700

    improve effieciency by assigning reused data to variable, reducing number of function calls within the function

--- eqObjects.js
+++ eqObjects.js
@@ -19,15 +19,18 @@ const assertEqual = function(actual, expected) {
 
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
     return false;
-  } for (const key of Object.keys(object1)) {
+  } for (const key of keys1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    }
-    else if (object1[key] !== object2[key]) {
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   } return true;
