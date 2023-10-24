6931e1c8215ab19d60debc3b662ff3bdaade20c4
Spencer Lewis <spence.914@gmail.com>
2023-10-24 13:02:55 -0700

    fix bug where function would return true after finding frist set of matching arrays, changed to return false when two different arrays are found instead

--- eqObjects.js
+++ eqObjects.js
@@ -23,8 +23,8 @@ const eqObjects = function(object1, object2) {
     return false;
   } for (const key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key])) {
-        return true;
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
       }
     }
     else if (object1[key] !== object2[key]) {
