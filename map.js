d1dc1ff4fe9fe26aa054f8cf8e0669d8b4e470fc
Spencer Lewis <spence.914@gmail.com>
2023-10-24 16:52:56 -0700

    create map function that takes an array and a callback function as arguments and performs the funciton on each element of the array

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  };
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+
+const nums = [1, 2, 3, 4];
+const results2 = map(nums, num => num * 2)
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
+assertArraysEqual(results2, [2, 4, 6, 8])
