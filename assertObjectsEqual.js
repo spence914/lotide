5bc4f94e5a4c477899d72628a7bd1de80cf5aaab
Spencer Lewis <spence.914@gmail.com>
2023-10-24 13:24:50 -0700

    create assertObjetsEqual function, add helper funcitons, run eqObjects inside of assertObjectsEqual, add util library's inpsect function to return more readable results to console

---
+++ assertObjectsEqual.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+
+const eqObjects = function(object1, object2) {
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  } for (const key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({name: "spencer", age: 30}, {name: "spencer", age: 30});
+assertObjectsEqual({name: "spencer", age: 30}, {name: "spencer", age: 32});
+assertObjectsEqual({}, {});
