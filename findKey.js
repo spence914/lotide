8eae2cfd135a741c3670b7cee6126a4d73433eec
Spencer Lewis <spence.914@gmail.com>
2023-10-25 14:02:39 -0700

    create findKey.js file, add assertion function, scope of function, test code

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Implement findKey function which takes an object and a callback, scans the object, and
+// returns the first key for which the callback returns a truthy value. If no key is found
+// it should return undefined
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
