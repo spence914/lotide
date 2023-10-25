0f4da250a30fa5baf21e661a74eacf4a48b3b20c
Spencer Lewis <spence.914@gmail.com>
2023-10-25 15:15:46 -0700

    implement solution and add more test cases, assign test object to variable to simplify adding test cases

--- findKey.js
+++ findKey.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -18,22 +18,53 @@ const assertEqual = function (actual, expected) {
 //     }
 //   }
 // };
-// I will use a similar for loop to go through the keys of the object and then similarly
-//check them against the callback for truthiness.
 
 
-const findKey = function(object, callback){
-for (const key of Object.keys(object)) {
-  if (object[key] === )
-}
-}
+// this function loops throught the keys of the passed function, then for each key checks if
+// a callback function passed on the object associated with that key returns a truthy response
+// if yes it returns the key itself, otherwise it returns undefined
+
+const findKey = function(object, callback) {
+  for (const key of Object.keys(object)) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
 
 
-findKey({
+const restaurants = {
   "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
+assertEqual(findKey(restaurants, x => x.stars === 4), undefined);
+assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
+
+const apartments = {
+  "Appt1": {
+    sqrft: 400,
+    bedrooms: 1,
+    bathrooms: 1
+  },
+  "Appt2": {
+    sqrft: 500,
+    bedrooms: 2,
+    bathrooms: 2
+  },
+  "Appt3": {
+    sqrft: 750,
+    bedrooms: 3,
+    bathrooms: 2.5
+  }
+};
+
+assertEqual(findKey(apartments, x => x.bathrooms > 2), "Appt3");
+assertEqual(findKey(apartments, x => x.bedrooms === 1), "Appt1");
+assertEqual(findKey(apartments, x => x.bedrooms > 3), undefined);
+
