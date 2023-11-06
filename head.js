f1cf17dcfb028f8a5624db63ce09bebad3e9d0b3
Spencer Lewis <spence.914@gmail.com>
2023-11-06 15:33:58 -0800

    move tests to dedicated test file, export function in module

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
