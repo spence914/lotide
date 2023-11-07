8cbf7b1d6ed96c6999ac28bb5e785be0a1686782
Spencer Lewis <spence.914@gmail.com>
2023-11-06 17:21:41 -0800

    move tests to dedicated file, export function as module, add comments for clarity

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,18 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+// This function will take a given sentence and return an object with a count of how many times each letter appeared
 
 const countLetters = function(sentence) {
   const results = {};
 
-  for (const letter of sentence) {
-    if (letter !== ' ') {
+  for (const letter of sentence) { // loop through all letters in sentence
+    if (letter !== ' ') { // ignore spaces
       if (results[letter]) {
-        results[letter] += 1;
+        results[letter] += 1; // if a key already exists in teh results object for a given letter increment it by one each time it is found
       } else {
-        results[letter] = 1;
+        results[letter] = 1; // if the key doesn't yet exist when the letter is found, create a key for it and initialize the count at 1
       }
     }
   }
   return results;
 };
 
-assertEqual(countLetters("lighthouse in the house").h , 4);
-assertEqual(countLetters("spencer ernest lewis")["s"] , 3);
-assertEqual(countLetters("spencer ernest lewis")[" "] , undefined);
-assertEqual(countLetters("      ")[" "] , undefined);
-assertEqual(countLetters("s")["s"] , 1);
+module.exports = countLetters;
