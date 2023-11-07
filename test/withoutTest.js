29d39a541678790077cd3235d31d9e6579f00b16
Spencer Lewis <spence.914@gmail.com>
2023-11-07 12:19:35 -0800

    refactor tests to use mocha and chai

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,9 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const without = require('../without');
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+describe("#without", () => {
+  it('should return ["hello", "world"] if given ["hello", "world", "lighthouse"],["lighthouse"]', () => {
+    assert.deepEqual(without(["hello", "world", "lighthouse"],["lighthouse"]), ["hello", "world"]);
+  });
+
+  it('should return ["1", "2"] if given ["1", "2", "3"], [1, 2, "3"]', () => {
+    assert.deepEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+  });
+});
