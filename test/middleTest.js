b83db02acedad14525c7afef30a7c3ddab15bb8a
Spencer Lewis <spence.914@gmail.com>
2023-11-06 18:08:32 -0800

    refactor tests to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,20 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]) , [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
-assertArraysEqual(middle([1, 2, 3, 4]) , [2,3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]) , [3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+// assertArraysEqual(middle([1, 2, 3, 4]) , [2,3]);
+
+describe("#middle", () => {
+  it("should return [3] when given [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it("should return [3,4] when give [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
+
+  it("should return [2,3] when given [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
