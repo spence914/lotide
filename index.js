54fed799282b496a82e23e51b07e827e6a202fd1
Spencer Lewis <spence.914@gmail.com>
2023-11-07 12:34:43 -0800

    create index file and export all functions through index module

---
+++ index.js
@@ -0,0 +1,26 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('../countLetters');
+const countOnly = require('../countOnly');
+const findKeyByValue = require('../findKeyByValue');
+const findKey = require('../findKey');
+const flatten = require('../flatten');
+const letterPositions = require('../letterPositions');
+const takeUntil = require('../takeUntil');
+const without = require('../without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
+};
