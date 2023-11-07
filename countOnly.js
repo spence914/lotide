b9c1825b691a2a8cafe86f7134f852101f3a688d
Spencer Lewis <spence.914@gmail.com>
2023-11-06 16:50:35 -0800

    export function as module, move tests to dedicated test file, add comments for clarity

--- countOnly.js
+++ countOnly.js
@@ -1,18 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+// Create a function that takes an array of items, and an item to count and returns the number of
+// times that item appears
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
-  for (const item of allItems) {
-    if (itemsToCount[item]) {
-      if (results[item]) {
+  for (const item of allItems) { // loops through all items in allItems
+    if (itemsToCount[item]) { // if the target item is found
+      if (results[item]) { // and a key for that item exists in results already it is incremented by 1
         results[item] += 1;
       } else {
-        results[item] = 1;
+        results[item] = 1; // if the item is found for the first time a key with its name is created and the value is initialized as 1
       }
     }
   }
@@ -20,22 +17,4 @@ const countOnly = function(allItems, itemsToCount) {
 };
 
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
