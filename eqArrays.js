b59d32e2ae5fc32a7eeef869e66333e1bfedfa5e
Spencer Lewis <spence.914@gmail.com>
2023-11-06 16:01:56 -0800

    export function as module, move tests to dedicated file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Write a function that compares two arrays to determine if they are equal
 
 // 1. Inputs -> Two arrays
@@ -30,19 +22,6 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-
-assertEqual(eqArrays([1, 2, [3, [4, [5]]]], [1, 2, [3, [4, [5]]]]), true);
-assertEqual(eqArrays([1, 2, [3, [4, [5]]]], [1, 2, [3, [4, [6]]]]), false);
+module.exports = eqArrays;
 
 
