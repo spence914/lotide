0653815987bc912b5c85b76d3e19951263985c7d
Spencer Lewis <spence.914@gmail.com>
2023-11-06 17:33:59 -0800

    move tests to dedicated file, export funciton as module

--- without.js
+++ without.js
@@ -1,27 +1,10 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 
 //Implement without function that takes an array and a set of unwanted elements to
 //remove from that array
 
-//Inputs: Two arrays, a complete one, and one containing unwated elements
+//Inputs: Two arrays, a complete one, and one containing unwanted elements
 //Outputs: A new array containing only the wanted elements from the first array
+
 //Process: Loop through the array comparing the elements and if they don't match the unwanted
 //ones them pushing them into a new array.
 //Above method didnt work because it only compared two elements at the same index, AI help
@@ -38,9 +21,4 @@ const without = function(sourceArray, itemsToRemove) {
 };
 
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+module.exports = without;
