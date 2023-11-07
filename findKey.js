fc8b1a91dc11cbd1227231a6669be2caf468b243
Spencer Lewis <spence.914@gmail.com>
2023-11-06 17:25:55 -0800

    move tests to dedicated file, export function as module

--- findKey.js
+++ findKey.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 // Implement findKey function which takes an object and a callback, scans the object, and
 // returns the first key for which the callback returns a truthy value. If no key is found
 // it should return undefined
@@ -23,38 +17,5 @@ const findKey = function(object, callback) {
 };
 
 
-const restaurants = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-};
-
-assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
-assertEqual(findKey(restaurants, x => x.stars === 4), undefined);
-assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
-
-const apartments = {
-  "Appt1": {
-    sqrft: 400,
-    bedrooms: 1,
-    bathrooms: 1
-  },
-  "Appt2": {
-    sqrft: 500,
-    bedrooms: 2,
-    bathrooms: 2
-  },
-  "Appt3": {
-    sqrft: 750,
-    bedrooms: 3,
-    bathrooms: 2.5
-  }
-};
-
-assertEqual(findKey(apartments, x => x.bathrooms > 2), "Appt3");
-assertEqual(findKey(apartments, x => x.bedrooms === 1), "Appt1");
-assertEqual(findKey(apartments, x => x.bedrooms > 3), undefined);
+module.exports = findKey;
 
