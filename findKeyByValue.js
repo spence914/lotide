fbabbe5585f05ae8810d13eeeac91a54313d38b0
Spencer Lewis <spence.914@gmail.com>
2023-11-06 17:02:01 -0800

    make dedicated test file, export funciton as module

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKeyByValue = function(object, value) {
   for (const key of Object.keys(object)) {
     if (object[key] === value) {
@@ -13,15 +7,4 @@ const findKeyByValue = function(object, value) {
 };
 
 
-
-
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+module.exports = findKeyByValue;
