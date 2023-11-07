e5bcfdc6e862314cee882d2c5666d99befd79805
Spencer Lewis <spence.914@gmail.com>
2023-11-06 16:04:39 -0800

    add comment to explain recursive aspect of function

--- eqArrays.js
+++ eqArrays.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) {
   }
   for (let i = 0; i < arr1.length; i++) {
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-      if (!eqArrays(arr1[i], arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) { // if the values at index i in both arrays are arrays themselves, recursively call eqArrays to determine equality
         return false;
       }
     } else if (arr1[i] !== arr2[i]) {
