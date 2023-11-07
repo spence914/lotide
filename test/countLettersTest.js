bfd12857e4724e3aef658f2497f9042c85308599
Spencer Lewis <spence.914@gmail.com>
2023-11-07 09:29:24 -0800

    refactor test code to mocha and chai

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,8 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const countLetters = require('../countLetters');
 
-assertEqual(countLetters("lighthouse in the house").h , 4);
-assertEqual(countLetters("spencer ernest lewis")["s"] , 3);
-assertEqual(countLetters("spencer ernest lewis")[" "] , undefined);
-assertEqual(countLetters("      ")[" "] , undefined);
-assertEqual(countLetters("s")["s"] , 1);
+// assertEqual(countLetters("lighthouse in the house").h , 4);
+// assertEqual(countLetters("spencer ernest lewis")["s"] , 3);
+// assertEqual(countLetters("spencer ernest lewis")[" "] , undefined);
+// assertEqual(countLetters("      ")[" "] , undefined);
+// assertEqual(countLetters("s")["s"] , 1);
+
+describe("#countLetters", () => {
+  it('returns 4 when given ("lighthouse in the house").h', () => {
+    assert.equal(countLetters("lighthouse in the house").h, 4);
+  });
+
+  it('returns undefined when given "     "[" "]', () => {
+    assert.equal(countLetters("     ")[" "], undefined);
+  });
+
+  it('returns 1 when given "s"["s"]', () => {
+    assert.equal(countLetters("s")["s"], 1);
+  });
+
+  it('returns undefined when given ("lighthouse labs")[" "]', () => {
+    assert.equal(countLetters("lighthouse labs")[" "], undefined);
+  });
+});
