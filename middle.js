0eb97ea79bf53f38efe83660821fd845859c357c
Spencer Lewis <spence.914@gmail.com>
2023-11-06 16:31:58 -0800

    clean up code by assigining midIndex to fullArray.length / 2 instead of having full calculation appear multiple times in code

--- middle.js
+++ middle.js
@@ -4,12 +4,13 @@
 
 const middle = function(fullArray) {
   let middleElements = []; // store middle elements in new array
+  let midIndex = fullArray.length / 2;
   if (fullArray.length < 3) {
     return middleElements; // if the array has fewer than 3 elements it has no middle and so the empty array is returned
   } else if (fullArray.length % 2 === 0) {
-    middleElements.push((fullArray[(fullArray.length / 2) - 1]) , fullArray[fullArray.length / 2]); // if the array has an even number of elements the middle two are pushed to the new array
+    middleElements.push((fullArray[midIndex - 1]) , fullArray[midIndex]); // if the array has an even number of elements the middle two are pushed to the new array
   } else {
-    middleElements.push(fullArray[Math.floor(fullArray.length / 2)]); // if the array has an odd number of elements only the one middle element is pushed
+    middleElements.push(fullArray[Math.floor(midIndex)]); // if the array has an odd number of elements only the one middle element is pushed
   } return middleElements;
 };
 
