d6a803216a42be5dc8a9fb6d225bbf2f5302a44d
Spencer Lewis <spence.914@gmail.com>
2023-11-07 09:55:35 -0800

    refactor tests to use mocha and chai

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,6 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const letterPositions = require('../letterPositions');
 
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("lighthouse labs").l, [0, 11]);
+// assertArraysEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").l, [2, 3]);
+// assertArraysEqual(letterPositions("lighthouse labs").l, [0, 11]);
+
+describe("#letterPositions", () => {
+  it('should return [1] when given ("hello").e', () => {
+    assert.deepEqual(letterPositions("hello").e, [1]);
+  });
+
+  it('should return [2,3] when given ("hello").l', () => {
+    assert.deepEqual(letterPositions("hello").l, [2, 3]);
+  });
+
+  it('should return [0,11] when given ("lighthouse labs").l', () => {
+    assert.deepEqual(letterPositions("lighthouse labs").l, [0, 11]);
+  });
+});
