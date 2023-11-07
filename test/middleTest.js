9cce4bf23f242b9566249ab267cbd8a74b709db3
Spencer Lewis <spence.914@gmail.com>
2023-11-06 18:10:53 -0800

    add test to ensure original array is not being modified by middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,7 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 
-// assertArraysEqual(middle([1, 2, 3, 4, 5]) , [3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
-// assertArraysEqual(middle([1, 2, 3, 4]) , [2,3]);
+
 
 describe("#middle", () => {
   it("should return [3] when given [1,2,3,4,5]", () => {
@@ -16,5 +14,12 @@ describe("#middle", () => {
 
   it("should return [2,3] when given [1,2,3,4]", () => {
     assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+  it("makes sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
   });
 });
