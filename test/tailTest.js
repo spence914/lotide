0804c0f1e894d4aa70350f5331c92e5a6832a422
Spencer Lewis <spence.914@gmail.com>
2023-11-06 18:03:08 -0800

    convert tests to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([1,2,3,4,5]).length,4);
-const words = ['Hello', 'this', 'is', 'a', 'test'];
-tail(words);
-assertEqual(words.length, 5);
+describe("#tail", () => {
+  it('should return [2,3,4,5] when given [1,2,3,4,5]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
 
-const result = tail(words);
-assertEqual(result.length, 4);
+  it("should return ['this', 'is', 'a', 'test'] when given ['Hello', 'this', 'is', 'a', 'test']", () => {
+    assert.deepEqual(tail(['Hello', 'this', 'is', 'a', 'test']), ['this', 'is', 'a', 'test']);
+  });
+});
