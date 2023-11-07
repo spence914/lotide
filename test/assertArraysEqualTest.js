18191fe4d151542447d25469c886e4d84f5ad031
Spencer Lewis <spence.914@gmail.com>
2023-11-07 13:22:37 -0800

    comment out non mocha/chai tests to clean up results of nmp test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
-assertArraysEqual([1,[2,3]],[1,[2,3]]); // Should pass
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
+// assertArraysEqual([1,[2,3]],[1,[2,3]]); // Should pass
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -3,19 +3,19 @@ const eqObjects = require('../eqObjects');
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+// assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+// assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+// assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+// assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
 
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
 
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
