21d51744659afdb8a3e1b2e2b969e1de0fc1a310
Spencer Lewis <spence.914@gmail.com>
2023-11-07 12:55:44 -0800

    fix file path error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const countLetters = require('../countLetters');
-const countOnly = require('../countOnly');
-const findKeyByValue = require('../findKeyByValue');
-const findKey = require('../findKey');
-const flatten = require('../flatten');
-const letterPositions = require('../letterPositions');
-const takeUntil = require('../takeUntil');
-const without = require('../without');
-const assertArraysEqual = require('../assertArraysEqual');
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
-const eqObjects = require('../eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
 
 
